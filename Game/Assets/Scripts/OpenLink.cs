56f1b77d34d51c4a2221d7683b215c695505a137
jessicattati <72874390+jessicattati@users.noreply.github.com>
2020-11-24 18:02:49 +0000

    added hyperlink

---
+++ Game/Assets/Scripts/OpenLink.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+
+    public void OpenChannel() {
+        Application.OpenURL("https://github.com/jessicattati/new-game");
+
+
+
+   }
+        
+    }
+
