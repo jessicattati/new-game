38727d5bba8a1c5eb8293d127ed9ab3ca71c149e
jessicattati <72874390+jessicattati@users.noreply.github.com>
2020-11-28 00:04:10 +0000

    hyperlink

---
+++ Game/Assets/Scripts/Link.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+    public class Link : MonoBehaviour
+    {
+
+    public void OpenGithub()
+    {
+        #if !UNITY_EDITOR
+        openWindow("https://github.com/jessicattati/new-game");
+        #endif
+    }
+    public void OpenBrowser()
+    {
+        #if !UNITY_EDITOR
+        openWindow("https://unity.com/");
+        #endif
+    }
+
+    [DllImport("__Internal")]
+    private static extern void openWindow(string url);
+    
+    
+}
